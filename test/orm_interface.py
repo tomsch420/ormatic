b88d5a96cff03fa5c26dfb086bca91d403e0d97c
David Prüser <david@pruesernet.de>
2025-06-18 16:57:22 +0200

    Updated explicit mapping inheritance

--- test/orm_interface.py
+++ test/orm_interface.py
@@ -79,9 +79,9 @@ t_Position = Table(
     Column('x', Float, nullable=False),
     Column('y', Float, nullable=False),
     Column('z', Float, nullable=False),
-    Column('positions_positions_id', ForeignKey('Positions.id')),
     Column('doublepositionaggregator_positions1_id', ForeignKey('DoublePositionAggregator.id')),
     Column('doublepositionaggregator_positions2_id', ForeignKey('DoublePositionAggregator.id')),
+    Column('positions_positions_id', ForeignKey('Positions.id')),
     Column('polymorphic_type', String(255))
 )
 
@@ -129,40 +129,40 @@ t_ObjectAnnotation = Table(
 
 mapper_registry = registry(metadata=metadata)
 
-m_Molecule = mapper_registry.map_imperatively(classes.example_classes.Molecule, t_Molecule, properties = dict(atoms=relationship('Atom',foreign_keys=[t_Atom.c.molecule_atoms_id]), 
-bonds=relationship('Bond',foreign_keys=[t_Bond.c.molecule_bonds_id])))
-
-m_Atom = mapper_registry.map_imperatively(classes.example_classes.Atom, t_Atom, )
-
-m_Position = mapper_registry.map_imperatively(classes.example_classes.Position, t_Position, polymorphic_on = "polymorphic_type", polymorphic_identity = "Position")
-
-m_Orientation = mapper_registry.map_imperatively(classes.example_classes.Orientation, t_Orientation, )
-
-m_ObjectAnnotation = mapper_registry.map_imperatively(classes.example_classes.ObjectAnnotation, t_ObjectAnnotation, properties = dict(object_reference=relationship('OriginalSimulatedObject',foreign_keys=[t_ObjectAnnotation.c.object_reference_id])))
-
-m_EnumContainer = mapper_registry.map_imperatively(classes.example_classes.EnumContainer, t_EnumContainer, )
-
-m_OGSimObjSubclass = mapper_registry.map_imperatively(classes.example_classes.OGSimObjSubclass, t_OGSimObjSubclass, properties = dict(pose=relationship('Pose',foreign_keys=[t_OGSimObjSubclass.c.pose_id]), 
-concept=t_OGSimObjSubclass.c.concept))
-
-m_Positions = mapper_registry.map_imperatively(classes.example_classes.Positions, t_Positions, properties = dict(positions=relationship('Position',foreign_keys=[t_Position.c.positions_positions_id])))
-
-m_PositionTypeWrapper = mapper_registry.map_imperatively(classes.example_classes.PositionTypeWrapper, t_PositionTypeWrapper, properties = dict(position_type=t_PositionTypeWrapper.c.position_type))
-
-m_Bond = mapper_registry.map_imperatively(classes.example_classes.Bond, t_Bond, properties = dict(atom1=relationship('Atom',foreign_keys=[t_Bond.c.atom1_id]), 
-atom2=relationship('Atom',foreign_keys=[t_Bond.c.atom2_id])))
+m_Node = mapper_registry.map_imperatively(classes.example_classes.Node, t_Node, properties = dict(parent=relationship('Node',foreign_keys=[t_Node.c.parent_id])))
 
 m_Pose = mapper_registry.map_imperatively(classes.example_classes.Pose, t_Pose, properties = dict(position=relationship('Position',foreign_keys=[t_Pose.c.position_id]), 
 orientation=relationship('Orientation',foreign_keys=[t_Pose.c.orientation_id])))
 
-m_Position4D = mapper_registry.map_imperatively(classes.example_classes.Position4D, t_Position4D, )
+m_Position = mapper_registry.map_imperatively(classes.example_classes.Position, t_Position, polymorphic_on = "polymorphic_type", polymorphic_identity = "Position")
 
-m_Node = mapper_registry.map_imperatively(classes.example_classes.Node, t_Node, properties = dict(parent=relationship('Node',foreign_keys=[t_Node.c.parent_id])))
+m_OriginalSimulatedObject = mapper_registry.map_imperatively(classes.example_classes.OriginalSimulatedObject, t_OriginalSimulatedObject, properties = dict(pose=relationship('Pose',foreign_keys=[t_OriginalSimulatedObject.c.pose_id]), 
+concept=t_OriginalSimulatedObject.c.concept))
+
+m_Position4D = mapper_registry.map_imperatively(classes.example_classes.Position4D, t_Position4D, )
 
 m_DoublePositionAggregator = mapper_registry.map_imperatively(classes.example_classes.DoublePositionAggregator, t_DoublePositionAggregator, properties = dict(positions1=relationship('Position',foreign_keys=[t_Position.c.doublepositionaggregator_positions1_id]), 
 positions2=relationship('Position',foreign_keys=[t_Position.c.doublepositionaggregator_positions2_id])))
 
-m_OriginalSimulatedObject = mapper_registry.map_imperatively(classes.example_classes.OriginalSimulatedObject, t_OriginalSimulatedObject, properties = dict(pose=relationship('Pose',foreign_keys=[t_OriginalSimulatedObject.c.pose_id]), 
-concept=t_OriginalSimulatedObject.c.concept))
+m_Molecule = mapper_registry.map_imperatively(classes.example_classes.Molecule, t_Molecule, properties = dict(atoms=relationship('Atom',foreign_keys=[t_Atom.c.molecule_atoms_id]), 
+bonds=relationship('Bond',foreign_keys=[t_Bond.c.molecule_bonds_id])))
+
+m_ObjectAnnotation = mapper_registry.map_imperatively(classes.example_classes.ObjectAnnotation, t_ObjectAnnotation, properties = dict(object_reference=relationship('OriginalSimulatedObject',foreign_keys=[t_ObjectAnnotation.c.object_reference_id])))
+
+m_Atom = mapper_registry.map_imperatively(classes.example_classes.Atom, t_Atom, )
+
+m_EnumContainer = mapper_registry.map_imperatively(classes.example_classes.EnumContainer, t_EnumContainer, )
+
+m_Orientation = mapper_registry.map_imperatively(classes.example_classes.Orientation, t_Orientation, )
+
+m_OGSimObjSubclass = mapper_registry.map_imperatively(classes.example_classes.OGSimObjSubclass, t_OGSimObjSubclass, properties = dict(pose=relationship('Pose',foreign_keys=[t_OGSimObjSubclass.c.pose_id]), 
+concept=t_OGSimObjSubclass.c.concept))
+
+m_PositionTypeWrapper = mapper_registry.map_imperatively(classes.example_classes.PositionTypeWrapper, t_PositionTypeWrapper, properties = dict(position_type=t_PositionTypeWrapper.c.position_type))
+
+m_Positions = mapper_registry.map_imperatively(classes.example_classes.Positions, t_Positions, properties = dict(positions=relationship('Position',foreign_keys=[t_Position.c.positions_positions_id])))
+
+m_Bond = mapper_registry.map_imperatively(classes.example_classes.Bond, t_Bond, properties = dict(atom1=relationship('Atom',foreign_keys=[t_Bond.c.atom1_id]), 
+atom2=relationship('Atom',foreign_keys=[t_Bond.c.atom2_id])))
 
 m_Position5D = mapper_registry.map_imperatively(classes.example_classes.Position5D, t_Position5D, polymorphic_identity = "Position5D", inherits = m_Position)
--- test/test_ormatic.py
+++ test/test_ormatic.py
@@ -229,9 +229,6 @@ class ORMaticTestCase(unittest.TestCase):
         with open('orm_interface.py', 'w') as f:
             ormatic.to_python_file(generator, f)
 
-        # with open('orm_interface.py', 'r') as f:
-        #     exec(f.read())
-
     def test_molecule(self):
         classes = [Atom, Bond, Molecule]
         ormatic = ORMatic(classes, self.mapper_registry)
@@ -328,6 +325,19 @@ class ORMaticTestCase(unittest.TestCase):
 
         og_sim_obj_sub = ormatic.class_dict[OGSimObjSubclass].mapped_table.local_table
         self.assertEqual(len(og_sim_obj_sub.columns), 3) #pose, concept, id (based on the explicit mapping)
+
+        og_sim = OGSimObjSubclass(Bowl(), Pose(Position(0, 0, 0), Orientation(0, 0, 0, 1)))
+        self.session.add(og_sim)
+        self.session.commit()
+
+        r = self.session.scalars(select(OGSimObjSubclass)).one()
+        self.assertEqual(r, og_sim)
+        self.assertIsInstance(r.concept, Bowl)
+        self.assertEqual(r.concept, og_sim.concept)
+        self.assertEqual(r.pose, og_sim.pose)
+        self.assertEqual(r.pose.position, og_sim.pose.position)
+        self.assertEqual(r.pose.orientation, og_sim.pose.orientation)
+        self.assertEqual(r.pose.orientation.w, 1)
 
     # def test_multiple_inheritance(self):
     #     classes = [Parent1, Parent2, MultipleInheritance]
